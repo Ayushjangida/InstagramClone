de076d41c73510cf5e17d9b7480bfa0ba31bf722
Ayush <ayushjangida30@gmail.com>
2020-08-13 17:03:42 +0530

    TabLayout setup added fragments to SocialMediaActivity

--- app/src/main/java/com/example/instagramclone/SocialMediaActivity.java
+++ app/src/main/java/com/example/instagramclone/SocialMediaActivity.java
@@ -1,14 +1,36 @@
 package com.example.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class SocialMediaActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private TabAdapter tabAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
+
+        setTitle("Social Media App!");
+
+        toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager = findViewById(R.id.view_pager);
+        tabAdapter = new TabAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(tabAdapter);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager, false);
+
     }
 }
