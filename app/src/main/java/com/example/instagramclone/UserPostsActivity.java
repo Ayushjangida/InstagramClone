882a92d49372dbffadc074f4c60301ae137739c6
Ayush <ayushjangida30@gmail.com>
2020-08-17 23:00:35 +0530

    Implements User Tab and User Activity Tab

--- app/src/main/java/com/example/instagramclone/UserPostsActivity.java
+++ app/src/main/java/com/example/instagramclone/UserPostsActivity.java
@@ -1,14 +1,92 @@
 package com.example.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.AsyncListUtil;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class UserPostsActivity extends AppCompatActivity {
+    private LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_posts);
+
+        linearLayout = findViewById(R.id.linear_layout);
+
+        Intent receivedIntentObject = getIntent();
+        final String receiveruserName = receivedIntentObject.getStringExtra("username");
+        Toast.makeText(this, "received " + receiveruserName, Toast.LENGTH_SHORT).show();
+
+        setTitle(receiveruserName + "'s posts");
+
+        ParseQuery<ParseObject> parseQuery = new ParseQuery<ParseObject>("photo");
+        parseQuery.whereEqualTo("username", receiveruserName);
+        parseQuery.orderByDescending("createdAt");
+        final ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setMessage("Loading...");
+        dialog.show();
+        parseQuery.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if(objects.size() > 0 && e == null) {
+                    for(ParseObject posts : objects)   {
+                        final TextView imageDes = new TextView(UserPostsActivity.this);
+                        imageDes.setText(posts.get("image_desc") + "");
+                        ParseFile postPicture = (ParseFile) posts.get("picture");
+                        postPicture.getDataInBackground(new GetDataCallback() {
+                            @Override
+                            public void done(byte[] data, ParseException e) {
+                                if(data != null && e == null)   {
+                                    Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                                    ImageView postImageView = new ImageView(UserPostsActivity.this);
+                                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                                    params.setMargins(5, 5, 5, 5);
+                                    postImageView.setLayoutParams(params);
+                                    postImageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                                    postImageView.setImageBitmap(bitmap);
+
+                                    LinearLayout.LayoutParams des_params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                                    des_params.setMargins(5, 5, 5, 5);
+                                    imageDes.setLayoutParams(des_params);
+                                    imageDes.setGravity(Gravity.CENTER);
+                                    imageDes.setBackgroundColor(Color.BLUE);
+                                    imageDes.setTextColor(Color.WHITE);
+                                    imageDes.setTextSize(30f);
+
+                                    linearLayout.addView(postImageView);
+                                    linearLayout.addView(imageDes);
+                                }
+                            }
+                        });
+                    }
+                }   else    {
+                    Toast.makeText(UserPostsActivity.this, "No posts for " + receiveruserName, Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                dialog.dismiss();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/instagramclone/UserTab.java
+++ app/src/main/java/com/example/instagramclone/UserTab.java
@@ -1,5 +1,6 @@
 package com.example.instagramclone;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,24 +8,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
+import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import libs.mjn.prettydialog.PrettyDialog;
+import libs.mjn.prettydialog.PrettyDialogCallback;
 
-public class UserTab extends Fragment {
+
+public class UserTab extends Fragment implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
 
 
     private ListView listView;
-    private ArrayList arrayList;
+    private ArrayList<String> arrayList;
     private ArrayAdapter arrayAdapter;
     private TextView txtLoadingData;
     public UserTab() {
@@ -52,6 +60,8 @@ public class UserTab extends Fragment {
         listView = view.findViewById(R.id.list_view_user);
         arrayList = new ArrayList();
         arrayAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, arrayList);
+        listView.setOnItemClickListener(UserTab.this);
+        listView.setOnItemLongClickListener(UserTab.this);
         listView.setVisibility(View.INVISIBLE);
         populateListView();
         return view;
@@ -76,5 +86,49 @@ public class UserTab extends Fragment {
                 }
             }
         });
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+      Intent intent = new Intent(getContext(), UserPostsActivity.class);
+      intent.putExtra("username", arrayList.get(position));
+      startActivity(intent);
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        final ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+        parseQuery.whereEqualTo("username", arrayList.get(position));
+        parseQuery.getFirstInBackground(new GetCallback<ParseUser>() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (user != null && e == null)  {
+                    final PrettyDialog pDialog = new PrettyDialog(getContext());
+                    try {
+                        pDialog
+                                .setTitle("User Info")
+                                .setMessage(String.valueOf(parseQuery.get("profe_bio")) +
+                                            parseQuery.get("profile_profession") +
+                                            parseQuery.get("profile_hobbies") +
+                                            parseQuery.get("profile_fav_sports"))
+                                .addButton(
+                                        "Cancel",
+                                        R.color.pdlg_color_white,
+                                        R.color.pdlg_color_red,
+                                        new PrettyDialogCallback() {
+                                            @Override
+                                            public void onClick() {
+                                                pDialog.dismiss();
+                                            }
+                                        }
+                                )
+                                .show();
+                    } catch (ParseException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
+        return false;
     }
 }
